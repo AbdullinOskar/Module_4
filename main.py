08f87df310530e15b2b9e522cbfcb8e5be1e38e8
Oskar <106113902+AbdullinOskar@users.noreply.github.com>
2024-03-29 04:00:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from new_m4_1 import function_1 as f1, function_2 as f2
+
+f1()
+print()
+f2()
---
+++ new_m4_1.py
@@ -0,0 +1,9 @@
+def function_1():
+    print('hello world')
+    print('Всем привет!!!')
+    print('Это моя первая функция')
+
+def function_2():
+    print('hello world')
+    print('Всем привет!!!')
+    print('Это моя вторая функция')
