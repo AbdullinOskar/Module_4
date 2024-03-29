82c7275c423a74f97123c3e86444a4f0776908ac
Oskar <106113902+AbdullinOskar@users.noreply.github.com>
2024-03-29 03:45:26 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,22 @@
+
+# import mod_1, mod_2
+#
+# print(mod_1.variable_1)
+#
+# mod_1.function_1()
+#
+# mod_2.function_1()
+
+# from mod_1 import function_1, variable_1
+# function_1()
+# print(variable_1)
+
+# from mod_1 import function_1 as f1, variable_1 as v1
+# f1()
+# print(v1)
+
+from mod_3 import function_1 as f1
+def function_1():
+    print('Hey')
+function_1()
+f1()
